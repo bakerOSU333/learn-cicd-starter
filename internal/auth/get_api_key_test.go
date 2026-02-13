762198c1eac4ead438891e832865a194c1061854
bakerOSU333 <jon.c.baker@gmail.com>
2026-02-13 08:26:14 -0600

    add formatting step to workflow

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -8,39 +8,39 @@ import (
 )
 
 func TestGetAPIKey(t *testing.T) {
-	tests := [] struct {
-		key string
-		value string
-		expect string
+	tests := []struct {
+		key       string
+		value     string
+		expect    string
 		expectErr string
 	}{
 		{
 			expectErr: "no authorization header",
 		},
 		{
-			key: "Authorization",
+			key:       "Authorization",
 			expectErr: "no authorization header",
 		},
 		{
-			key: "Authorization",
-			value: "-",
+			key:       "Authorization",
+			value:     "-",
 			expectErr: "malformed authorization header",
 		},
 		{
-			key: "Authorization",
-			value: "Bearer xxxxxx",
+			key:       "Authorization",
+			value:     "Bearer xxxxxx",
 			expectErr: "malformed authorization header",
 		},
 		{
-			key: "Authorization",
-			value: "ApiKey xxxxxx",
-			expect: "xxxxxx",
+			key:       "Authorization",
+			value:     "ApiKey xxxxxx",
+			expect:    "xxxxxx",
 			expectErr: "not expecting an error",
 		},
 	}
 
 	for i, test := range tests {
-		t.Run(fmt.Sprintf("TestGetAPIKey Case #%v:", i), func(t * testing.T) {
+		t.Run(fmt.Sprintf("TestGetAPIKey Case #%v:", i), func(t *testing.T) {
 			header := http.Header{}
 			header.Add(test.key, test.value)
 
